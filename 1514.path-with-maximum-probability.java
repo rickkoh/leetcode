94f30796a3247fdba5dd0eb6fb15b0222aea1d8e
Rick <rick.kohjiaxuan@gmail.com>
2024-09-01 00:40:30 +0800

    Refactor graph representation for Dijkstra's algorithm

--- 1514.path-with-maximum-probability.java
+++ 1514.path-with-maximum-probability.java
@@ -11,30 +11,20 @@ import java.util.PriorityQueue;
 
 class Solution {
 
-    static class Edge {
-        int node;
-        double prob;
-
-        public Edge(int node, double prob) {
-            this.node = node;
-            this.prob = prob;
-        }
-    }
-
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
 
-        List<List<Edge>> adjList = new ArrayList<>();
+        List<double[]>[] adjList = new ArrayList[n];
 
         for (int i = 0; i < n; i++) {
-            adjList.add(new ArrayList<>());
+            adjList[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < edges.length; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
             double prob = succProb[i];
-            adjList.get(u).add(new Edge(v, prob));
-            adjList.get(v).add(new Edge(u, prob));
+            adjList[u].add(new double[] { v, prob });
+            adjList[v].add(new double[] { u, prob });
         }
 
         PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[1], a[1]));
@@ -54,11 +44,12 @@ class Solution {
                 return prob;
             }
 
-            for (Edge edge : adjList.get(node)) {
-                double new_prob = prob * edge.prob;
-                if (new_prob > weights[edge.node]) {
-                    weights[edge.node] = new_prob;
-                    pq.add(new double[] { edge.node, new_prob });
+            for (double[] edge : adjList[node]) {
+                node = (int) edge[0];
+                double new_prob = prob * edge[1];
+                if (new_prob > weights[node]) {
+                    weights[node] = new_prob;
+                    pq.add(new double[] { node, new_prob });
                 }
             }
 

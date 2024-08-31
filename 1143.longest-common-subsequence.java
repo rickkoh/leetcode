913376de572e954628b45244dc1fb781c30cc00d
Rick <rick.kohjiaxuan@gmail.com>
2024-08-31 12:34:04 +0800

    Attempt

---
+++ 1143.longest-common-subsequence.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1143 lang=java
+ *
+ * [1143] Longest Common Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        
+    }
+}
+// @lc code=end
+

7c75317fb3833c1593d7a4eac2a7d7eb0321c941
Rick <rick.kohjiaxuan@gmail.com>
2024-09-06 10:24:02 +0800

    Complete

--- 1143.longest-common-subsequence.java
+++ 1143.longest-common-subsequence.java
@@ -7,8 +7,34 @@
 // @lc code=start
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        
+        int n = text1.length();
+        int m = text2.length();
+        int[][] memo = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return branchNext(text1, text2, n - 1, m - 1, memo);
+    }
+
+    private int branchNext(String text1, String text2, int i, int j, int[][] memo) {
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        if (text1.charAt(i) == text2.charAt(j)) {
+            memo[i][j] = 1 + branchNext(text1, text2, i - 1, j - 1, memo);
+        } else {
+            memo[i][j] = Math.max(branchNext(text1, text2, i - 1, j, memo),
+                    branchNext(text1, text2, i, j - 1, memo));
+        }
+
+        return memo[i][j];
     }
 }
 // @lc code=end
-

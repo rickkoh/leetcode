49f62a7d5ee461cc3b780390bac8f5a084221b06
Rick <rick.kohjiaxuan@gmail.com>
2024-09-10 16:27:05 +0800

    improve performance insert-greatest-common-divisors-in-linked-list

--- 2807.insert-greatest-common-divisors-in-linked-list.java
+++ 2807.insert-greatest-common-divisors-in-linked-list.java
@@ -28,16 +28,12 @@ class Solution {
     }
 
     private int gcd(int a, int b) {
-        if (a == b) {
-            return a;
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-
-        if (a > b) {
-            return gcd(a - b, b);
-        } else {
-            return gcd(a, b - a);
-        }
-
+        return a;
     }
 }
 // @lc code=end

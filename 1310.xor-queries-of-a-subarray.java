aca721065c60f0868f8f34d949e0ad37c4f059d6
Rick <rick.kohjiaxuan@gmail.com>
2024-09-13 10:16:49 +0800

    Improve runtime

--- 1310.xor-queries-of-a-subarray.java
+++ 1310.xor-queries-of-a-subarray.java
@@ -7,12 +7,6 @@
 // @lc code=start
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        // int[] ans = new int[queries.length];
-        // for (int i = 0; i < queries.length; i++) {
-        // ans[i] = xor(arr, queries[i][0], queries[i][1]);
-        // }
-        // return ans;
-
         int[] prefixXor = new int[arr.length];
 
         prefixXor[0] = arr[0];
@@ -33,13 +27,5 @@ class Solution {
 
         return ans;
     }
-
-    // public int xor(int[] arr, int c, int e) {
-    // if (c == e) {
-    // return arr[c];
-    // } else {
-    // return arr[c] ^ xor(arr, c + 1, e);
-    // }
-    // }
 }
 // @lc code=end

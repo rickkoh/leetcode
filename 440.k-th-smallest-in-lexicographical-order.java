7a792bb6a4a104b37942e5b62cfc5e5544bd2bbb
Rick <rick.kohjiaxuan@gmail.com>
2024-09-22 23:11:37 +0800

    Complete 440.k-th-smallest-in-lexicographical-order.java

---
+++ 440.k-th-smallest-in-lexicographical-order.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=440 lang=java
+ *
+ * [440] K-th Smallest in Lexicographical Order
+ */
+
+// @lc code=start
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            int step = countSteps(n, curr, curr + 1);
+            if (step <= k) {
+                curr++;
+                k -= step;
+            } else {
+                curr *= 10;
+                k--;
+            }
+        }
+
+        return curr;
+    }
+
+    private int countSteps(int n, long prefix1, long prefix2) {
+        int steps = 0;
+        while (prefix1 <= n) {
+            steps += Math.min(n + 1, prefix2) - prefix1;
+            prefix1 *= 10;
+            prefix2 *= 10;
+        }
+        return steps;
+    }
+}
+// @lc code=end

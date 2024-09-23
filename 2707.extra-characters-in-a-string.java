1f4de79f1480fc595a324f7ffe3b9aeb72f47142
Rick <rick.kohjiaxuan@gmail.com>
2024-09-23 13:58:46 +0800

    Complete 2707.extra-characters-in-a-string.java

---
+++ 2707.extra-characters-in-a-string.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=2707 lang=java
+ *
+ * [2707] Extra Characters in a String
+ */
+
+// @lc code=start
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        int n = s.length();
+        HashSet<String> set = new HashSet<>();
+        for (String word : dictionary) {
+            set.add(word);
+        }
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            StringBuilder cur = new StringBuilder();
+            int mini = n;
+            for (int idx = i; idx < n; idx++) {
+                cur.append(s.charAt(idx));
+                int curSize = set.contains(cur.toString()) ? 0 : cur.length();
+                int nextSize = dp[idx + 1];
+                mini = Math.min(mini, curSize + nextSize);
+            }
+            dp[i] = mini;
+        }
+        return dp[0];
+    }
+}
+// @lc code=end

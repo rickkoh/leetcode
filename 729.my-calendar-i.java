24ee42d27a878faef47f144351320d1294f75937
Rick <rick.kohjiaxuan@gmail.com>
2024-09-26 16:22:36 +0800

    Complete 729.my-calendar-i.java

---
+++ 729.my-calendar-i.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=729 lang=java
+ *
+ * [729] My Calendar I
+ */
+
+// @lc code=start
+class MyCalendar {
+    TreeMap<Integer, Integer> calendar;
+
+    MyCalendar() {
+        calendar = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+        Integer prev = calendar.floorKey(start),
+                next = calendar.ceilingKey(start);
+        if ((prev == null || calendar.get(prev) <= start) &&
+                (next == null || end <= next)) {
+            calendar.put(start, end);
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
+// @lc code=end
